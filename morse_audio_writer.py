5981dd358210dc36a0cde96566f8105ff95ae05c
Gerben Erens <grbn24@gmail.com>
2021-03-07 01:17:47 +0100

    Added docstrings

--- morse_audio_writer.py
+++ morse_audio_writer.py
@@ -1,6 +1,8 @@
-import numpy as np
+"""This module adds functions to convert text to morse code, morse code to time sequence morse code, and time sequence morse code to audio files"""
 import sys
+import numpy as np
 import wavio
+
 
 CODE = {' ': '_',
 	"'": '.----.',
@@ -51,10 +53,11 @@ CODE = {' ': '_',
 	'Z': '--..',
 	'_': '..--.-'}
 
-def convert_to_morse_code(sentence, print_morse = False):
-    sentence = sentence.upper()
+def convert_to_morse_code(sen, print_morse = False):
+    """Function converts string to morse code, returning morse code"""
+    sen = sen.upper()
     encoded_sentence = ""
-    for character in sentence:
+    for character in sen:
         if character != " ":
             encoded_sentence += CODE[character] + " "
         else:
@@ -63,7 +66,8 @@ def convert_to_morse_code(sentence, print_morse = False):
         print("Sentence in morse: " + encoded_sentence)
     return encoded_sentence
 
-def convert_morse_to_audio(morse):
+def convert_morse_to_time_seq(morse):
+    """Function converts morse string to time sequence, where a 1 would be a sound, and a 0 would be no sound"""
     morse = morse.replace(" ", "0 0 0 ")
     morse = morse.replace("/", "0 0 0 0 0 0 0 ")
     morse = morse.replace(".", "1 0 ")
@@ -72,6 +76,7 @@ def convert_morse_to_audio(morse):
     return out
 
 def write_morse_audio(aud, filename = "0_sentence.wav"):
+    """Function writes time sequence where 1 is tone, and 0 is silence to a .wav audio file"""
     dt = 0.5        # seconds per timeunit
     rate = 44100    # samples per second
     rep = round(rate*dt)
@@ -85,17 +90,18 @@ def write_morse_audio(aud, filename = "0_sentence.wav"):
     wavio.write(filename, x, rate, sampwidth=3)
 
 def mainfun(sen):
-    write_morse_audio(convert_morse_to_audio(convert_to_morse_code(sen,print_morse=True)))
-    woorden = sen.split(" ")
+    write_morse_audio(convert_morse_to_time_seq(convert_to_morse_code(sen,print_morse=True)))
+    words = sen.split(" ")
     n = 1
-    for w in woorden:
-        write_morse_audio(convert_morse_to_audio(convert_to_morse_code(w)), filename=(str(n)+"_"+w+".wav"))
+    for w in words:
+        w_morse = convert_to_morse_code(w)
+        write_morse_audio(convert_morse_to_time_seq(w_morse), filename=(str(n)+"_"+w+".wav"))
         n += 1
 
 if __name__ == "__main__":
-	sentence = "This is morse code"
-	if len(sys.argv) > 1:
-		s_list = sys.argv[1:]
-		sentence = " ".join(map(str,s_list))
-	mainfun(sentence)
+    sentence = "This is morse code"
+    if len(sys.argv) > 1:
+        s_list = sys.argv[1:]
+        sentence = " ".join(map(str,s_list))
+    mainfun(sentence)
     
